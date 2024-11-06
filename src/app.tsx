b0be03d873b5b527e6b2f0cb265fd321fc1f6411
Titus Moore <tmoore@edgewebware.com>
2024-11-06 17:13:04 -0500

    Added resize sidebar

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,26 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
+import { Resizable, ResizablePanel, ResizableHandle } from "~/components/ui/resizable";
 import "./app.css";
 
 export default function App() {
   return (
     <Router
       root={props => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
+        <Resizable
+          orientation="horizontal"
+          class="h-full w-full min-h-screen min-w-screen"
+        >
+          <ResizablePanel initialSize={.2}>
+            My sidebar container
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel initialSize={.8}>
+            <Nav />
+            <Suspense>{props.children}</Suspense>
+          </ResizablePanel>
+        </Resizable>
       )}
     >
       <FileRoutes />
