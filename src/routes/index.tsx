8f1a6bcdd1c43d59ddfce45f6e73a706a6878ebf
Titus Moore <tmoore@edgewebware.com>
2024-11-06 16:42:11 -0500

    Moved where shadcn was being used

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { A } from "@solidjs/router";
 import Counter from "~/components/Counter";
-import { Button } from "~/components/ui/button";
 
 export default function Home() {
   return (
@@ -13,7 +12,6 @@ export default function Home() {
           solidjs.com
         </a>{" "}
         to learn how to build Solid apps.
-        <Button>Testing</Button>
       </p>
       <p class="my-4">
         <span>Home</span>
